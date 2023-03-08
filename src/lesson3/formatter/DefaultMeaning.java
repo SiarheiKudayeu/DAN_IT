30ba754a9103441b2795091412532107e8e81e1f
siarhei <greysiarhei@gmail.com>
2023-03-08 08:15:43 +0300

    Add class DefaultMeaning.java

---
+++ src/lesson3/formatter/DefaultMeaning.java
@@ -0,0 +1,25 @@
+package lesson3.formatter;
+
+public class DefaultMeaning {
+    static int x;
+    static byte b;
+    static short s;
+    static long l;
+    static char c;
+    static float f;
+    static double d;
+    static boolean bo;
+    static String str;
+
+    public static void main(String[] args) {
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(x);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bo);
+        System.out.println(str);
+    }
+}
