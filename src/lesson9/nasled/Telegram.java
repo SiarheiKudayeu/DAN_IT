15ad7ffa38f259d13e254abc0cbdc61275e3edd7
siarhei <greysiarhei@gmail.com>
2023-03-23 19:57:41 +0300

    nasledovanie, constructorer

--- src/lesson9/nasled/Telegram.java
+++ src/lesson9/nasled/Telegram.java
@@ -1,30 +1,35 @@
 package lesson9.nasled;
 
-public class Telegram extends Messenger{
-    public Telegram(int space){
-        super("Telegram", 1.0,space);
+public class Telegram extends Messenger {
+    public Telegram(int space) {
+        super("Telegram", 1.0, space);
     }
-    public Telegram(){
+
+    public Telegram() {
         super();
     }
 
-    public void launch(){
+    @Override
+    public void launch() {
         System.out.println("Ваш Telegram запущен");
     }
-    public void shutDown(){
+
+    public void shutDown() {
         System.out.println("Ваш Telegram закрыт");
     }
-    public void shutDown(String s){
+
+    public void shutDown(String s) {
         System.out.println("Ваш Telegram закрыт" + s);
     }
 
-    public boolean canMakeVideCall(){
+    public boolean canMakeVideCall() {
         System.out.println("Я могу делать видео звонок");
         return true;
     }
-    public void getInfo(){
-        System.out.println("Имя приложения "+super.getName());
-        System.out.println("Версия приложения "+super.getVersion());
-        System.out.println("Размер приложения "+super.getSpace());
+
+    public void getInfo() {
+        System.out.println("Имя приложения " + super.getName());
+        System.out.println("Версия приложения " + super.getVersion());
+        System.out.println("Размер приложения " + super.getSpace());
     }
 }
